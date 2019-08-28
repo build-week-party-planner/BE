44872e6b6d4c667e8e9656b778b00118ea76f6f8
Sydney Crumley <srsimps19@gmail.com>
2019-08-28 12:02:02 -0500

    maybe fixed persist issue

--- api/db/dbConfig.js
+++ api/db/dbConfig.js
@@ -3,5 +3,7 @@ const knex = require("knex");
 //* import knex configuration
 const knexConfig = require('../../knexfile');
 
+const dbEnv = process.env.DB_ENV || 'development';
+
 //* create and export database/knex connection
 module.exports = knex(knexConfig.development);
